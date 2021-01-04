be08ff7ce0d29fdf34b22020d9180ab083aec1c0
Cody Lemons <clemons9988@gmail.com>
2021-01-04 09:38:06 -0600

    Add files via upload

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,58 @@
-$(document).ready(function() {
-  $("#dropdown-content").hide();
-
-  $("#panel").click(function() {
-    $("#dropdown-content").slideToggle("slow");
+  $(document).ready(function() {
+    $('#panel').on('click', function() {
+      $('#content').toggle(1000);
+    });
   });
 
-});
+    let noon = 12;
+
+    let showCurrentTime = function () {
+      let localTime = document.getElementById('local-time');
+      let message = document.getElementById('content-message');
+      let currentTime = new Date();
+
+      let hours = currentTime.getHours();
+      let minutes = currentTime.getMinutes();
+      let meridian;
+
+      if (hours <= 11) {
+        message.innerText = "Good Morning!";
+        meridian = "AM";
+      } else {
+        meridian = "PM";
+      }
+
+      if (hours >= 18) {
+        message.innerText = "Good Evening!";
+      }
+
+      if (hours >= noon && hours < 18) {
+        message.innerText = "Good Afternoon!";
+      }
+
+      if (hours > noon) {
+        hours = hours - 12;
+      }
+
+      if (minutes < 10) {
+        minutes = "0" + minutes;
+      }
+
+      let clockTime = hours + ":" + minutes + " " + meridian + " " + "(CST)";
+      localTime.innerText = clockTime;
+    };
+
+    showCurrentTime();
+
+    let updateClock = function() {
+      let time = new Date().getMinutes();
+
+    };
+
+    updateClock();
+
+    let oneMinute = 60000;
+    setInterval(updateClock, oneMinute);
+
+ 
+  
