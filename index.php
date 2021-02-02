a99738c0f7332b45164e8eccbdd6885fd1778efb
Cody Lemons <clemons9988@gmail.com>
2021-02-02 15:10:47 -0600

    Add files via upload

--- index.php
+++ index.php
@@ -31,6 +31,10 @@
 
   </head>
   <body>
+    <div id= "aboutbutton" class="aboutbtn center" style="text-align:center;">About Me</div>
+      <div id="content" class="responsive"><span><span id="content-message"></span><br><br>I graduated from the <em>University of Mississippi</em> in 2012 with a B.A. in Journalism. In 2017, I went on to study Web Development through <em>Bloc.io</em>. Now, I spend my time building websites and experimenting with code. If you're a writer or programmer like me, I'd love to hear from you. So please find me on Twitter and Github.</span>
+    </div>
+
     <ul id="social-links">
       <li id="social"><a href="http://www.twitter.com/cody_lemons" target="_blank" class="btn btn-block btn-social btn-twitter">
       <span class="fa fa-twitter"></span><p></p>
@@ -42,103 +46,143 @@
       <span class="fa fa-book"></span><p></p>
       </a></li>
     </ul> 
-    <!--<img src="images/sunshine-cody.jpeg" alt="Cody Lemons and his wife Sunshine taking a selfie and smiling" height="420" width="310"/>-->
-    <button id="panel">About Me</button>
-      <div id="content"><span><span id="content-message"></span><br><br>I graduated from the <em>University of Mississippi</em> in 2012 with a B.A. in Journalism. In 2017, I went on to study Web Development through <em>Bloc.io</em>. Now, I spend my time building websites and experimenting with code. If you're a writer or programmer like me, I'd love to hear from you. So please send me a message or find me on Twitter and Github.</span>
+  <br>
+  <br>
+  <br>
+  <br>
+
+<div class="slideshow-container">
+  
+<div class="code-header header"><h3>Coding Projects</h3></div>
+
+  <div class="mySlides fade">
+    <img class="projectimage" src="images/project1.png">
+    <div class="text">Food Ordering App With Cart & Menu!<br><span class="sourcecode"><em style="font-size:10px; color:white;"><a href="https://github.com/Clemons09/Sunshines-Kitchen" target="_blank" style="text-decoration:none; color:antiquewhite;">Click for Source Code</em></span></div>
+    
+  </div>
+  <div class="mySlides fade">
+    <img class="projectimage" src="images/project2.png">
+    <div class="text">ECommerce App With Advanced Cart Features!</div>
+  </div>
+  <div class="mySlides fade">
+    <img class="projectimage" src="images/project3.png">
+    <div class="text">Blog/Forum with Sign-in/Sign-out Features</div>
+  </div>
+
+  <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+  <a class="next" onclick="plusSlides(1)">&#10095;</a>
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span>
+  <span class="dot" onclick="currentSlide(2)"></span>
+  <span class="dot" onclick="currentSlide(3)"></span>
+</div>
+
+<hr>
+ <br><br>
+ <br><br>
+    <!-- Trigger the Modal -->
+    <div class="prosamples header"><h3>Professional Writing Samples</h3></div>
+
+<img id="myImg" src="images/article1.jpg" style="width:100%;max-width:300px">
+
+<!-- The Modal -->
+<div id="myModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img01">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>
+
+
+    <!-- Trigger the Modal -->
+<img id="mySecondImg" src="images/article2.jpg" style="width:100%;max-width:300px">
+<!-- The Modal -->
+<div id="mySecondModal" class="modal">
+  <!-- The Close Button -->
+  <span class="close2">&times;</span>
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img02">
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>
+    <!-- Trigger the Modal -->
+    <img id="myThirdImg" src="images/article3.jpg" style="width:100%;max-width:300px">
+
+<!-- The Modal -->
+<div id="myThirdModal" class="modal">
+
+  <!-- The Close Button -->
+  <span class="close3">&times;</span>
+
+  <!-- Modal Content (The Image) -->
+  <img class="modal-content" id="img03">
+
+  <!-- Modal Caption (Image Text) -->
+  <div id="caption"></div>
+</div>
+    <div class="clearfix"></div>
     </div>
-  <br>
-  <br>
-  <br>
-  <div class="wsheader"><h3>Professional Writing Samples</h3></div>
-    <div class="writingsamples">
-      <div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="images/article1.jpg">
-      <img src="images/article1.jpg" alt="first published article from 2009" width="600" height="400">
-    </a>
-    <div class="desc">First published article (2009) - <em>The Daily Mississippian</em></div>
-  </div>
-</div>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="images/article2.jpg">
-      <img src="images/article2.jpg" alt="second published article from 2012" width="600" height="400">
-    </a>
-    <div class="desc">Second published article (2012) - <em>DeSoto Times-Tribune</em></div>
-  </div>
-</div>
-<div class="responsive">
-  <div class="gallery">
-    <a target="_blank" href="images/article3.jpg">
-      <img src="images/article3.jpg" alt="third published article from 2012" width="600" height="400">
-    </a>
-    <div class="desc">Third published article (2012) - <em>DeSoto Times-Tribune</em></div>
-  </div>
-</div>
-<div class="clearfix"></div>
-</div> <br><br>
-    <div class="wsaheader"><h3>Short Stories Available on Amazon!</h3></div>
+    <hr>
+
+    <div class="shorts-header"><h3>Short Stories on Amazon!</h3></div>
     <div class="writings-amazon">
-    <div class="resp-amazon">
+      <div class="resp-amazon">
       <div class="gall-amazon">
         <a target="_blank" href="https://t.co/qei2bCAAF4?amp=1">
-          <img src="images/mental.jpg" alt="outline of a head facing to the right with jumbled lines inside the head to show confusion" width="600" height="400">
+          <div class="tooltip"><img class="leakimage" src="images/mental.jpg" alt="outline of a head facing to the right with jumbled lines inside the head to show confusion" width="600" height="400"></div>
         </a>
-        <div class="description-amazon">"The Leak" - <em>Available on Amazon!</em></div>
-        <!--<span id="info-icon" class="iconify" data-icon="bi:info-circle" data-inline="false"></span>-->
+        <div class="description-amazon"><strong>"The Leak"</strong> - <em>Available on Amazon!</em></div>
       </div>
     </div>
     <div class="resp-amazon">
       <div class="gall-amazon">
         <a target="_blank" href="https://t.co/QNyYSGXPno?amp=1">
-          <img src="images/prison.jpg" alt="image of a prisoner's silhouette walking down a dark prison corridor alone" width="600" height="400">
+          <img class="encoreimage" src="images/prison.jpg" alt="image of a prisoner's silhouette walking down a dark prison corridor alone" width="600" height="400">
         </a>
-        <div class="description-amazon">"Encore! A Prison Population Story" - <em>Available on Amazon!</em></div>
-        <!--<span id="info-icon" class="iconify" data-icon="bi:info-circle" data-inline="false"></span>-->
+        <div class="description-amazon"><strong>"Encore! A Prison Population Story"</strong> - <em>Available on Amazon!</em></div>
       </div>
     </div>
     <div class="clearfix"></div>
     </div>
-    <div class="container">
+    <!--<div class="container">
       <form id="email-form" class="contact-form" action="contactform.php" method="POST">
       <fieldset>
         <legend>Send me a message:</legend>
     <div id="name-field" class="form-group">
       <input class="form-control" type="text" name="name" placeholder="Full name">
-      <span class="error">* <?php echo $nameErr;?></span>
       <br>
     </div>
     <div id="email-field" class="form-group">
       <input class="form-control" type="text" name="mail" placeholder="Your email">
-      <span class="error">* <?php echo $emailErr;?></span>
       <br>
     </div>
     <div id="subject-field" class="form-group">
       <input class="form-control" type="text" name="subject" placeholder="Subject">
-      <span class="error">* <?php echo $subjectErr;?></span>
       <br>
     </div>
     <div class="form-group">
     <br>
       <textarea class="form-control" id="message-field" name="message" placeholder="Message"></textarea>
-      <span class="error">* <?php echo $messageErr;?></span>
+      
       <br>
     </div>
       <button id="btn1" type="submit" name="submit">Send</button>
       <button id="btn2" type="reset" name="reset">Clear</button>
-      <?php
-        function function_alert($successmsg) {
-          if ( isset($_GET['success']) && $_GET['success']==true )
-          echo "<script>alert('$successmsg');</script>";
-        }
-        function_alert("Message Sent!");
-        ?>
     </fieldset>
     </form>
-    </div>
+    </div>-->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/prefixfree/1.0.7/prefixfree.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <script type="text/javascript" src="scripts/index.js"></script>
+    
   </body>
       <p class="copyright">Cody Lemons &copy; 2021</p>
