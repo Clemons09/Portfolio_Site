bfb4a28acc6e84e91f323ba0232b8f04d29e8a66
Cody Lemons <clemons9988@gmail.com>
2021-02-13 14:01:24 -0600

    Add files via upload
    made a few updates to site

--- app.js
+++ app.js
@@ -2,16 +2,19 @@
 $(document).ready(function() {
   $("#fixings1").hide();
   $(".fa-times").hide();
+
   $(".fa-bars").click(function() {
     $("#fixings1").toggle("slow");
     $(".fa-bars").hide();
+    $(".dropdown-content").show("slow");
     $(".headline").hide();
     $(".fa-times").show();
   });
   $(".fa-times").click(function() {
     $("#fixings1").toggle("slow");
     $(".fa-times").hide();
-    $(".headline").show();
+    $(".dropdown-content").hide();
+    $(".headline").show(1000);
     $(".fa-bars").show();
   });
 });
